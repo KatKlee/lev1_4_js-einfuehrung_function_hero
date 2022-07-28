c0c46f2757c6c4ebde3f14cacf3ebe49621d1590
KatKlee <katklee2022@gmail.com>
2022-07-28 15:39:59 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function hero(heroName, heroPower, heroEnemy) {
+    let name = 'Mein Lieblingsheld aus Marvel ist: ' + heroName + '.'
+    let power = ' Er hat die Fähigkeit: ' + heroPower + '.'
+    let enemy = ' Sein größter Gegner ist: ' + heroEnemy + '.'
+    console.log(name + power + enemy)
+}
+
+hero('Peter Pan', 'knutschen', 'Einsamkeit')
